35f59e91b78a6ff51f75231b826efed303f9ebff
Isra42069 <154467725+Isra42069@users.noreply.github.com>
2023-12-20 22:48:12 +0100

    Create subtask2.py

---
+++ subtask2.py
@@ -0,0 +1,17 @@
+n = 0
+s = 0
+m = float('inf')  # set initial minimum to positive infinity
+a = -1
+x = int(input("Enter a number (-1 to terminate): "))
+while x != -1:
+n += 1
+s += x
+m = min(m, x)
+x = int(input("Enter a number (-1 to terminate): ")
+if n != 0:
+ a = s / n
+print(f"Count (n): {n}")
+print(f"Sum (s): {s}")
+print(f"Minimum (m): {m}")
+print(f"Mean (a): {a}")
+
